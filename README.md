f569587c7babbfae21ec93d9c690078e660743f3
filipeusz <filip.zieleniak@gmail.com>
2023-10-28 00:36:42 +0200

    CLONE_NEWUTS namespace

---
+++ README.md
@@ -0,0 +1,10 @@
+## Container at home
+
+Exec into container:
+
+`go run container-at-home.go run /bin/bash`
+
+### Namespaces
+
+1. CLONE_NEWUTS - isolated hostname
+
---
+++ container-at-home.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "os"
+    "os/exec"
+    "fmt"
+    "syscall"
+)
+
+func main() {
+    switch os.Args[1] {
+    case "run":
+        run()
+    default:
+        panic("help")
+    }
+}
+
+func run() {
+    fmt.Printf("Running %v \n", os.Args[2:])
+    
+    cmd := exec.Command(os.Args[2], os.Args[3:]...)
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+
+    cmd.SysProcAttr = &syscall.SysProcAttr {
+        Cloneflags: syscall.CLONE_NEWUTS,
+    }
+
+    must(cmd.Run())
+}
+
+func must(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
